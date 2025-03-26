94401819bc36e6d00936486089168eff5adc4820
chrtbe <chrtine.bell2003@gmail.com>
2025-03-26 12:50:47 +0100

    Fix styling

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: "export",
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? "/portfolio" : "",
+  assetPrefix: isProd ? "/portfolio/" : "",
 };
 
 export default nextConfig;
