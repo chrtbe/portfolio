9cb6237477dfbeeeb9d5f11fb9869f74081ee7ca
chrtbe <chrtine.bell2003@gmail.com>
2025-03-26 13:34:05 +0100

    Fix styling

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: isProd ? "/portfolio.github.io" : "",
-  assetPrefix: isProd ? "" : "",
+  assetPrefix: isProd ? "/portfolio.github.io/" : "",
 };
 
 export default nextConfig;
