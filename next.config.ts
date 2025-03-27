1daa338fb129d2e9a849e18c8c80bf9ae0f5d59a
chrtbe <chrtine.bell2003@gmail.com>
2025-03-27 12:14:07 +0100

    Fix images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig = {
   output: "export",
   images: {
     loader: "default",
+    path: isProd ? "/portfolio/" : "",
   },
   basePath: isProd ? "/portfolio" : "",
   assetPrefix: isProd ? "/portfolio" : "",
