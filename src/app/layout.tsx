96ab7b6b6ada08a56f1585b062fab19693dcfc80
chrtbe <chrtine.bell2003@gmail.com>
2025-03-27 22:25:37 +0100

    Remove unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Lilita_One } from "next/font/google";
+import { Geist, Lilita_One } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
